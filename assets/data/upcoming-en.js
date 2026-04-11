dc52371f51d3b2c328ee81ea02bedb49d829d97c
Il-Custode <donciccio3@hotmail.it>
2026-04-11 11:08:37 +0200

    Update upcoming-en.js

--- assets/data/upcoming-en.js
+++ assets/data/upcoming-en.js
@@ -1,1 +1,8 @@
-
+window.arcaneUpcomingEn = [
+  {
+    title: "New Free Download",
+    description: "A new free resource designed to introduce the editorial style of THE ARCANE SHOP.",
+    status: "In preparation",
+    type: "Free Downloads"
+  }
+];
