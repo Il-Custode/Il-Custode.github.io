4812fa10628587b12927426dd2a9dec4c00acdac
Il-Custode <donciccio3@hotmail.it>
2026-04-14 19:23:58 +0200

    Create home-featured-it.js

---
+++ assets/data/home-featured-it.js
@@ -0,0 +1,16 @@
+window.arcaneHomeFeaturedIt = [
+  {
+    id: "crimson-ladys-feast",
+    label: "In evidenza",
+    title: "The Crimson Lady's Feast",
+    description: "Una one-shot horror gotica investigativa tra feste in maschera, segreti di sangue e finali multipli, progettata per offrire tensione elegante, alta rigiocabilità e possibili seguiti di campagna.",
+    link: "https://site.dmsguild.com/product/564224/The-Crimson-Ladys-Feast?src=newest_in_dmg&filters=45469"
+  },
+  {
+    id: "bastion-compendium",
+    label: "In evidenza",
+    title: "Compendio dei Bastioni",
+    description: "Un’espansione completa e modulare dedicata ai Bastioni, pensata per ampliare profondamente l’esperienza di gioco.",
+    link: "https://site.dmsguild.com/product/563364/The-Definitive-Bastion-Compendium-for-DD-5e"
+  }
+];
