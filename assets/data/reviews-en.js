f07a32175a81da59c7db493b86b47b317cd11ec4
Il-Custode <donciccio3@hotmail.it>
2026-04-14 21:33:28 +0200

    Create reviews-en.js

---
+++ assets/data/reviews-en.js
@@ -0,0 +1,16 @@
+window.arcaneReviewsEn = [
+  {
+    category: "master",
+    productId: "",
+    stars: 5,
+    comment: "Fantastic session, immersive atmosphere, and excellent table management.",
+    author: "Verified customer"
+  },
+  {
+    category: "product",
+    productId: "the-definitive-bastion-compendium-for-dd-5e",
+    stars: 5,
+    comment: "Very polished product, practical, and full of useful ideas ready for play.",
+    author: "Verified customer"
+  }
+];
